f72a638df971416f12fdf243a36fe50b245c29c9
nunez.micaela-soc <nunez.micaela@pucp.edu.pe>
2026-06-22 01:03:25 -0500

    Initial commit

---
+++ Creacion y enlace Git.R
@@ -0,0 +1,14 @@
+#=============================================
+#Práctica Calificada 3: Datos de la ENAHO sobre "Actividad agropecuaria" 
+#Autora: Micaela Nuñez Cordero
+#Objetivo de script: Crear sistema de carpetas y enlazar con GitHub
+#Fecha: 21 junio del 2026
+#==============================================================
+#Creamos carpetas------------------------------------------
+dir.create("datos")
+dir.create("datos/crudos")
+dir.create("datos/procesados")
+dir.create("outputs")
+dir.create("docs")
+
+#Enlace con Git y Github
