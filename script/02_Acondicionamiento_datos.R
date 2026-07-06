35ebaf978fbb33418c28c92c860813ede5eb1e72
nunez.micaela-soc <nunez.micaela@pucp.edu.pe>
2026-07-06 01:58:57 -0500

    Esta parte parece simple, porque las variables ya han sido consolidadas en la PC3, por lo que es lógico que no tengan NAs. Sin embargo lo que falta es recodificar los codigos especiales y revisar los tipos de datos.

--- script/02_Acondicionamiento_datos.R
+++ script/02_Acondicionamiento_datos.R
@@ -89,4 +89,35 @@ names(enaho)
 glimpse(enaho)
 summary(enaho)
 
+# 2. DIAGNÓSTICO DE NAs Y REPORTE-----------------------------------------------
+# ------------------------------------------------------------------------------
+
+# 2.1 Visualización Gráfica (naniar)
+# Creamos un gráfico de barras que muestra la cantidad de NAs por variable
+library(naniar)
+grafico_nas <- gg_miss_var(enaho, show_pct = TRUE) +
+  labs(
+    title = "Porcentaje de Valores Perdidos (NAs) por Variable",
+    subtitle = "Proyecto: Análisis de consumo en los hogares peruanos usando datos de la ENAHO (2025)",
+    y = "% de Valores Perdidos",
+    x = "Variables"
+  ) +
+  theme_minimal()
+
+# Mostramos el gráfico en el panel de RStudio
+print(grafico_nas)
+
+# Exportamos el gráfico a nuestra carpeta de outputs
+ggsave("outputs/Grafico_NAs_ENAHO.png", plot = grafico_nas, 
+       width = 8, height = 6, bg = "white")
+
+# 2.2 Reporte Tabular
+# Calculamos el % de NAs por variable y lo guardamos en CSV
+reporte_nas <- enaho %>%
+  summarise(across(everything(), ~ round(sum(is.na(.)) / n() * 100, 2))) %>%
+  pivot_longer(everything(), names_to = "variable", values_to = "porcentaje_na") %>%
+  arrange(desc(porcentaje_na))
+
+write_csv(reporte_nas, "outputs/Reporte_Datos_Perdidos_ENAHO.csv")
+
 
