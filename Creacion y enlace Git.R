e68758d03b525898310baf0f90733b1eb47ea08d
nunez.micaela-soc <nunez.micaela@pucp.edu.pe>
2026-07-12 18:45:39 -0500

    movi un texto

--- Creacion y enlace Git.R
+++ Creacion y enlace Git.R
@@ -2,7 +2,7 @@
 #Trabajo final . Taller de Procesamiento de Datos  
 #Autora: Micaela Nuñez Cordero
 #Objetivo de script: Crear sistema de carpetas y enlazar con GitHub
-# Módulos: Gastos del hogar en alimentos y bebidas (600); servicios a la vivienda (605); esparcimiento, diversión y servicios de cultura (606); bienes y servicios de cuidado personal (606D)
+#Módulos: Gastos del hogar en alimentos y bebidas (600); servicios a la vivienda (605); esparcimiento, diversión y servicios de cultura (606); bienes y servicios de cuidado personal (606D)
 #Fecha: 12 de julio del 2026
 #==============================================================
 #Creamos carpetas------------------------------------------
