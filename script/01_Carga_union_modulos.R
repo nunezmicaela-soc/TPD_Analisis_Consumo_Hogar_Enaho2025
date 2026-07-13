ad4f88bcebc59b8c695d79ec51ec365aa1425787
nunez.micaela-soc <nunez.micaela@pucp.edu.pe>
2026-07-12 19:04:25 -0500

    importación y unión de módulos ENAHO 2025: - módulo 601 (alimentos y bebidas) y 606 (cultura/esparcimiento) como base inicial - incorporación de módulo 605 (servicios a la vivienda) y 606D (cuidados personales) para ampliar el análisis de gasto del hogar - objetivo: consolidar una base única a nivel hogar que permita explorar patrones de consumo y preparar la clasificación posterior

--- script/01_Carga_union_modulos.R
+++ script/01_Carga_union_modulos.R
@@ -2,7 +2,7 @@
 #Trabajo final . Taller de Procesamiento de Datos  
 #Autora: Micaela Nuñez Cordero
 #Objetivo de script: Cargar los módulos, gestionar y hacer joins
-#Módulos: Gastos del hogar en alimentos y bebidas (600); servicios a la vivienda (605); esparcimiento, diversión y servicios de cultura (606); bienes y servicios de cuidado personal (606D)
+#Módulos: Gastos del hogar en alimentos y bebidas (601); servicios a la vivienda (605); esparcimiento, diversión y servicios de cultura (606); bienes y servicios de cuidado personal (606D)
 #Fecha: 12 de julio del 2026
 #=====================================================================
 
@@ -15,10 +15,16 @@ library(arrow)
 library(naniar)
 
 
-#2. Definir ruta de datos 
-library(rio)
+#2. Definir ruta de datos-------------------------------------------
+ruta_crudos <- "datos/crudos/"
+ruta_procesados <- "datos/procesados/"
+
+#3. Importar módulos------------------------------------
+
 mod601 <- import("datos/crudos/Enaho01-2025-601.csv", encoding="Latin-1")
 mod606 <- import("datos/crudos/Enaho01-2025-606.csv", encoding="Latin-1")
+mod605 <- import("datos/crudos/Enaho01-2025-605.csv", encoding="Latin-1")
+mod606D <- import("datos/crudos/Enaho01-2025-606D.csv", encoding="Latin-1")
 
 #3. Union de bases--------------------------
 keys_hogar <- c("AÑO", "MES", "CONGLOME", "VIVIENDA", "HOGAR","UBIGEO", "DOMINIO", "ESTRATO", "NCONGLOME", "SUB_CONGLOME")
