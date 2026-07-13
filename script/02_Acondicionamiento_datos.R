68bbfd675786e8d3c511421e1b8a3c1a3e7f5f54
nunez.micaela-soc <nunez.micaela@pucp.edu.pe>
2026-07-12 23:11:13 -0500

    Primero cargué la base de datos que se creo en el script anterior. He eliminado algunos codigos anteriores para volver a ordenar e incluir los nuevos módulos.

--- script/02_Acondicionamiento_datos.R
+++ script/02_Acondicionamiento_datos.R
@@ -1,10 +1,9 @@
 # ==============================================================================
-# Proyecto: Análisis de consumo en los hogares peruanos con datos de la ENAHO
-# Script: Acondicionamiento 
-# Autor: Micaela Nuñez 
-# Fecha: 05-07-2026
-# Objetivo: Acondicionar la base de datos consolidada (Tipado, Selección, 
-#           Renombrado, Tratamiento de NAs).
+#Trabajo final . Taller de Procesamiento de Datos  
+#Autora: Micaela Nuñez Cordero
+#Objetivo de script: Acondicionar la base de datos consolidada (Tipado, selección, renombrado, tratamiento de NAs).
+#Módulos: Gastos del hogar en alimentos y bebidas (601); servicios a la vivienda (605); esparcimiento, diversión y servicios de cultura (606); bienes y servicios de cuidado personal (606D)
+#Fecha: 12 de julio del 2026
 # ==============================================================================
 
 # ------------------------------------------------------------------------------
@@ -14,15 +13,6 @@
 # Restaurar entorno del proyecto (renv)
 renv::restore()
 
-#Instalar paquetes 
-install.packages("rio")
-install.packages ("tidyverse")
-install.packages("arrow")
-install.packages("janitor")
-install.packages("pkgbuild")
-install.packages("Rcpp")
-install.packages("naniar")
-
 # Cargar librerías principales
 library(tidyverse)
 library(rio)       # import/export
@@ -30,34 +20,15 @@ library(arrow)     # parquet
 library(janitor)   # limpieza de nombres
 library(naniar)    # diagnóstico de valores perdidos
 
-
-# Verificar que Rtools está disponible 
-pkgbuild::has_build_tools(debug = TRUE)
-
-# Actualizar snapshot de renv cuando ya todo está instalado
-renv::snapshot()
-
 # ------------------------------------------------------------------------------
-# 1. CARGA, SELECCIÓN, RENOMBRADO Y DIAGNÓSTICO---------------------------------
+# 1. CARGA Y SELECCIÓN DE VARIABLES---------------------------------
 # ------------------------------------------------------------------------------
-# Leemos la base consolidada de la PC3
-library(tidyverse)
-library(arrow)
-enaho_raw <- read_parquet("datos/procesados/enaho_2025_230626.parquet")
+# Leemos la base consolidada creada en el script pasado
 
-#Prueba de seleccion y renombrado de variables 
-enaho <- enaho_raw %>%
-  select(AÑO, MES, UBIGEO, gasto_alimentos, gasto_cultura) %>%
-  rename(
-    year = AÑO,
-    month = MES,
-    region = UBIGEO,
-    alimentos = gasto_alimentos,
-    cultura = gasto_cultura
-  )
-names(enaho_raw)
+enaho_raw <- read_parquet("datos/procesados/enaho_2025_uniongastos.parquet")
+
 -----------------------------------------------------------------------------
-# 1. SELECCIÓN, RENOMBRADO Y DIAGNÓSTICO
+# 2. SELECCIÓN, RENOMBRADO Y DIAGNÓSTICO
 # ------------------------------------------------------------------------------
 #Seleccion y renombre
 enaho <- enaho_raw %>%
@@ -70,17 +41,19 @@ enaho <- enaho_raw %>%
     ubigeo   = UBIGEO,
     dominio  = DOMINIO,
     estrato  = ESTRATO,
-    nconglome = NCONGLOME,
-    subconglome = SUB_CONGLOME,
     gasto_alimentos,
-    gasto_cultura
+    gasto_cultura,
+    gasto_vivienda,
+    gasto_cuidados
   ) %>%
   rename(
-    year     = año,
-    month    = mes,
-    region   = ubigeo,
+    year      = año,
+    month     = mes,
+    region    = ubigeo,
     alimentos = gasto_alimentos,
-    cultura   = gasto_cultura
+    cultura   = gasto_cultura,
+    vivienda  = gasto_vivienda,
+    cuidados_personales  = gasto_cuidados
   )
 
 # Inspección inicial
